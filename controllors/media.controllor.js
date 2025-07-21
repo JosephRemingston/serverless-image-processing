806ad9ae0c24fb2d8f0e08226e8091821f27661c
JosephRemingston <ljremi@gmail.com>
2025-07-22 00:28:00 +0530

    initial commit

--- controllors/media.controllor.js
+++ controllors/media.controllor.js
@@ -2,7 +2,6 @@ var express = require('express');
 var ApiError = require('../utils/ApiError');
 var ApiResponse = require('../utils/ApiResponse');
 var asyncHandler = require('../utils/asyncHandler');
-var removeContentTypeFromUrl = require("../utils/urlGenerator.js");
 var getSecrets = require("../utils/aws-secrets");
 const { v4: uuidv4 } = require('uuid');
 var AWS = require('aws-sdk');
@@ -13,35 +12,42 @@ AWS.config.update({
     secretAccessKey : secrets.AWS_SECRETACCESSKEY,
     region : "ap-southeast-1"
 });
+
 var s3 = new AWS.S3();
 
+function sanitizeEmail(email) {
+    return email.replace(/[@.]/g, "_");
+}
 
 const generateSignedUrl = asyncHandler(async(req, res) => {
     if (!req.user || !req.user.username) {
         throw new ApiError(401, "User not authenticated");
     }
-    
-    const userId = req.user.username;
-    const fileName = `${userId}.png`;
 
-    try{
-        var params = {
-            Bucket : "serverless-media-processing-upload",
-            Key : `uploads/${fileName}`,
-            ContentType : "image/png"
-        }
+    const emailSafe = sanitizeEmail(req.user.username);
+    const uniqueId = uuidv4();
 
+    const fileName = `${emailSafe}_${uniqueId}.png`;
 
-        var url = await s3.getSignedUrlPromise("putObject" , params);
+    try {
+        const params = {
+            Bucket: "serverless-media-processing-upload",
+            Key: `uploads/${fileName}`,
+            ContentType: "image/png",
+            Expires: 300 // URL valid for 5 mins
+        };
 
-        return ApiResponse.success(res , "signed url generated" , {
-            "signed url" : url,
-            "filename" : fileName,
-        })
+        const url = await s3.getSignedUrlPromise("putObject", params);
+
+        return ApiResponse.success(res, "Signed URL generated", {
+            signedUrl: url,
+            fileName: fileName,
+            uploadKey: params.Key // Useful if you need to reference it later
+        });
+    } catch (error) {
+        console.error("Error generating signed URL:", error);
+        return ApiResponse.error(res, error.statusCode || 400, error.message, error);
     }
-    catch(error){
-        return ApiResponse.error(res , error.statusCode || 400 , error.message , error);
-    }
-})
+});
 
 module.exports = { generateSignedUrl };
